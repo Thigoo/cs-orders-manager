6c01003ff82711d55bd84b1dae505ec90502eadd
Thiago Garcia <thiago.garcia@tupimob.com>
2025-03-08 15:52:04 -0300

    feat: more configs

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+export const NavBar = () => {
+  return (
+    <div className="flex justify-between">
+      <div>Logo</div>
+      
+      <div>
+        <ul>
+          <li>Home</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
